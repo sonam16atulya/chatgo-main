1dd78f41f3066c38e4a261a66a847e2813020aa6
Sonam <146235259+sonam16atulya@users.noreply.github.com>
2023-10-06 20:46:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <div className = "chat-box">
+        <h1>Chat Go</h1>
+        <p>Send WhatsApp message without saving contact.</p>
+        <input></input>
+      </div>
+    </div>
+  );
+}
+
+export default App;
